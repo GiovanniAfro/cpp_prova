b5448e3be9a0b7ebde1ae17a09a0153f6b951cd9
Giovanni Cavanna <giovanni.cacciacavanna@gmail.com>
2023-10-25 17:45:56 +0200

    prove

--- stdio_stream.cpp
+++ stdio_stream.cpp
@@ -6,13 +6,13 @@
 /*   By: gcavanna <gcavanna@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 16:56:55 by gcavanna          #+#    #+#             */
-/*   Updated: 2023/10/25 17:02:00 by gcavanna         ###   ########.fr       */
+/*   Updated: 2023/10/25 17:45:17 by gcavanna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main(void)
+/* int main(void)
 {
     char    buff[512];
 
@@ -21,5 +21,18 @@ int main(void)
     std::cin >> buff;
     std::cout << "You entered: [" << buff << "]" << std::endl;
 
+    return 0;
+} */
+
+int main(void)
+{
+    int numero;
+
+    std::cout << "inserisci un numero" << std::endl;
+    std::cin >> numero;
+    if (numero < 0)
+        std::cerr << "numero negativo non ci piace" << std::endl;
+    else
+        std::cout << "hai messo: [" <<numero<<"]" << std::endl;
     return 0;
 }
