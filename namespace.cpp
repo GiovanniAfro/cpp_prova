6afcd915b261371eb3ceddb1910fb817a623644a
Giovanni Cavanna <giovanni.cacciacavanna@gmail.com>
2023-10-25 18:12:01 +0200

    aggiunto commenti

--- namespace.cpp
+++ namespace.cpp
@@ -6,7 +6,7 @@
 /*   By: gcavanna <gcavanna@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 15:16:35 by gcavanna          #+#    #+#             */
-/*   Updated: 2023/10/25 15:29:03 by gcavanna         ###   ########.fr       */
+/*   Updated: 2023/10/25 18:11:39 by gcavanna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,3 +44,13 @@ int main(void)
     printf("gl_var:     [%d]\n", ::gl_var);
     printf("f();        [%d]\n\n", ::f());
 }
+
+/* L'operatore di risoluzione dello scope (::), noto come "operatore di risoluzione di ambito",
+ è utilizzato in C++ per accedere agli elementi (come variabili, funzioni o classi) all'interno di un namespace o di una classe. 
+ Questo operatore è fondamentale per risolvere i possibili conflitti di nomi all'interno di un programma.
+
+Ecco come funziona:
+
+    Namespace:
+    Quando si dichiara uno "namespace", è possibile includere al suo interno variabili, funzioni, classi e altro ancora. 
+    Per accedere a questi elementi all'interno del namespace, si utilizza l'operatore di risoluzione dello scope (::) seguito dal nome dell'elemento. */
--- stdio_stream.cpp
+++ stdio_stream.cpp
@@ -6,7 +6,7 @@
 /*   By: gcavanna <gcavanna@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 16:56:55 by gcavanna          #+#    #+#             */
-/*   Updated: 2023/10/25 17:45:17 by gcavanna         ###   ########.fr       */
+/*   Updated: 2023/10/25 18:10:39 by gcavanna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,3 +36,31 @@ int main(void)
         std::cout << "hai messo: [" <<numero<<"]" << std::endl;
     return 0;
 }
+
+
+/* gli "stdio streams" sono concetti associati all'input e all'output dei dati, e si riferiscono principalmente a tre stream standard:
+1. **stdin**:
+   - `stdin` è l'abbreviazione di "standard input" (input standard).
+   - Rappresenta il flusso di input standard da cui un programma C++ può leggere dati.
+   - Di solito, `stdin` è collegato alla tastiera, il che significa che puoi leggere dati dalla tastiera utilizzando `cin` in C++.
+
+2. **stdout**:
+   - `stdout` è l'abbreviazione di "standard output" (output standard).
+   - Rappresenta il flusso di output standard in cui un programma C++ può scrivere dati.
+   - Di solito, `stdout` è collegato al display (console), il che significa che puoi scrivere dati sullo schermo utilizzando `cout` in C++.
+
+3. **stderr**:
+   - `stderr` è l'abbreviazione di "standard error" (errore standard).
+   - Rappresenta il flusso di output standard per gli errori o messaggi di errore.
+   - Di solito, `stderr` è collegato al display (console) come `stdout`, ma viene utilizzato per stampare messaggi di errore in modo che siano chiaramente distinguibili dai dati normali.
+
+ */
+
+/* std::endl è una sequenza di escape utilizzata principalmente con lo stream di output standard (std::cout) per inserire una nuova riga nel testo che viene scritto. 
+Questo è utile per formattare l'output in modo più leggibile, in quanto separa i dati in righe diverse.
+Ecco cosa fa std::endl:
+1.Inserisce una nuova riga: Quando std::endl viene utilizzato con std::cout (il flusso di output standard), scrive un carattere di ritorno a capo (newline) nel testo. 
+Questo fa sì che il testo successivo verrà scritto su una nuova riga, rendendo l'output più organizzato.
+2.Svuota il buffer: Inoltre, std::endl forza il buffer di output a essere svuotato. 
+Il buffer è una memoria temporanea utilizzata per accumulare dati prima che vengano effettivamente scritti su un dispositivo di output. Usando std::endl,
+ puoi garantire che i dati siano immediatamente scritti sulla console o su un file, invece di aspettare che il buffer si riempia. */
