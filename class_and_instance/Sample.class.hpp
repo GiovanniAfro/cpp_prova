b8746378c5f8a4ff2340769f7484cdf7c56cf7c1
Giovanni Cavanna <giovanni.cacciacavanna@gmail.com>
2023-10-27 15:45:30 +0200

    prove

--- class_and_instance/Sample.class.hpp
+++ class_and_instance/Sample.class.hpp
@@ -6,12 +6,15 @@
 /*   By: gcavanna <gcavanna@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 15:04:39 by gcavanna          #+#    #+#             */
-/*   Updated: 2023/10/27 15:17:35 by gcavanna         ###   ########.fr       */
+/*   Updated: 2023/10/27 15:42:10 by gcavanna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SAMPLE_CLASS_H
 # define SAMPLE_CLASS_H
+
+#include <iostream>
+#include <string>
 
 class Sample
 {
@@ -20,6 +23,19 @@ public:
     
     Sample(void);
     ~Sample(void);
+};
+
+class Macchina
+{
+
+public:
+    std::string marca;
+    int anno;
+
+    //funzione membro
+    void  accelera(){
+        std::cout << "L'auto sta accelerando!" << std::endl;
+    }
 };
 
 #endif
--- class_and_instance/main.cpp
+++ class_and_instance/main.cpp
@@ -6,15 +6,27 @@
 /*   By: gcavanna <gcavanna@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 15:09:10 by gcavanna          #+#    #+#             */
-/*   Updated: 2023/10/27 15:09:51 by gcavanna         ###   ########.fr       */
+/*   Updated: 2023/10/27 15:44:40 by gcavanna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Sample.class.hpp"
 
-int main(void)
+/* int main(void)
 {
     Sample instance;
+
+    return 0;
+} */
+
+int main(void)
+{
+    Macchina mia;
+    Sample joejoe;
+
+    mia.marca = "Ferrari";
+    mia.anno = 2022;
+    mia.accelera();
 
     return 0;
 }
