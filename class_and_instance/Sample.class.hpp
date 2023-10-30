9936e2a77cdcb691b41005bb0a764dea8235bdc8
Giovanni Cavanna <giovanni.cacciacavanna@gmail.com>
2023-10-30 12:13:31 +0100

    sus

--- class_and_instance/Sample.class.hpp
+++ class_and_instance/Sample.class.hpp
@@ -6,7 +6,7 @@
 /*   By: gcavanna <gcavanna@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 15:04:39 by gcavanna          #+#    #+#             */
-/*   Updated: 2023/10/27 16:25:50 by gcavanna         ###   ########.fr       */
+/*   Updated: 2023/10/27 16:48:36 by gcavanna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,3 +43,9 @@ public:
 };
 
 #endif
+
+/* Gli attributi di membro e le funzioni di membro sono componenti fondamentali delle classi in C++, 
+consentono di rappresentare dati e comportamenti legati a oggetti della classe. 
+Gli attributi rappresentano le informazioni e le caratteristiche degli oggetti, 
+mentre le funzioni rappresentano ciò che gli oggetti possono fare o le operazioni che possono essere eseguite su di essi.
+ */
